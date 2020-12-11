b480556e16433e732e412b9de11dd4cdff6db62d
mila98matf <61909262+mila98matf@users.noreply.github.com>
2020-12-11 21:35:50 +0000

    Update aplikacija.py

--- aplikacija.py
+++ aplikacija.py
@@ -12,7 +12,42 @@ class Gui(QtWidgets.QWidget):
         self.show()
 
     def initGui(self):
-  #komentar
+        self.setGeometry(0,0,1000,1000)
+        
+        vbox = QtWidgets.QVBoxLayout()
+        self.setLayout(vbox)
+        
+        #dodajemo sliku u pozadini
+        oImage = QImage("2.jpeg")
+        sImage = oImage.scaled(QSize(500,500))                   
+        palette = QPalette()
+        palette.setBrush(QPalette.Window, QBrush(sImage))
+        self.setPalette(palette)
+
+        self.licni = QLabel("LICNI PODACI", self)  
+        vbox.addWidget(self.licni)
+
+        hbox1 = QtWidgets.QHBoxLayout()
+        lGodine= QLabel("Uneti broj godina: ")
+        tGodine= QtWidgets.QLineEdit()
+        hbox1.addWidget(lGodine)
+        hbox1.addWidget(tGodine)
+        vbox.addLayout(hbox1)
+
+        self.simptomi = QLabel("SIMPTOMI", self)  
+        vbox.addWidget(self.simptomi)
+
+        #temperatura
+        hboxT = QtWidgets.QHBoxLayout()
+        ltemp= QLabel("Temperatura: ")
+        rtemp1= QtWidgets.QRadioButton("manje od 37")
+        rtemp2= QtWidgets.QRadioButton("od 37 do 38")
+        rtemp3= QtWidgets.QRadioButton("vise od 38")
+        hboxT.addWidget(ltemp)
+        hboxT.addWidget(rtemp1)
+        hboxT.addWidget(rtemp2)
+        hboxT.addWidget(rtemp3)
+        vbox.addLayout(hboxT)
 
 
 app = QtWidgets.QApplication([])
