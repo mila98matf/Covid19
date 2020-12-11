0a3e5fedb4242e9378b84b121fa95ddd3f6d392c
mila98matf <61909262+mila98matf@users.noreply.github.com>
2020-12-11 21:17:08 +0000

    pocetni fajl

---
+++ aplikacija.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QImage, QPalette, QBrush
+
+
+
+class Gui(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initGui()
+        self.show()
+
+    def initGui(self):
+
+
+
+app = QtWidgets.QApplication([])
+window = Gui()
+app.exec_()
