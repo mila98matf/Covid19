80beef9ef2232b28917df1c0e1e7adf635605955
jovanaMATF <61909258+jovanaMATF@users.noreply.github.com>
2020-12-12 09:46:13 +0100

    Dodavanje simptoma

--- aplikacija.py
+++ aplikacija.py
@@ -36,6 +36,8 @@ class Gui(QtWidgets.QWidget):
 
         self.simptomi = QLabel("SIMPTOMI", self)  
         vbox.addWidget(self.simptomi)
+        self.cesti = QLabel("Najcesci simptomi",self)
+        vbox.addWidget(self.cesti) 
 
         #temperatura
         hboxT = QtWidgets.QHBoxLayout()
@@ -48,6 +50,87 @@ class Gui(QtWidgets.QWidget):
         hboxT.addWidget(rtemp2)
         hboxT.addWidget(rtemp3)
         vbox.addLayout(hboxT)
+        
+        #suv kasalj
+        hboxSK = QtWidgets.QHBoxLayout()
+        lskasalj= QLabel("Suv kasalj: ")
+        rskasalj1= QtWidgets.QRadioButton("Da")
+        rskasalj2= QtWidgets.QRadioButton("Ne")
+        hboxSK.addWidget(lskasalj)
+        hboxSK.addWidget(rskasalj1)
+        hboxSK.addWidget(rskasalj2)
+        vbox.addLayout(hboxSK)
+
+        #umor
+        hboxU = QtWidgets.QHBoxLayout()
+        lumor= QLabel("Umor: ")
+        rumor1= QtWidgets.QRadioButton("Da")
+        rumor2= QtWidgets.QRadioButton("Ne")
+        hboxU.addWidget(lumor)
+        hboxU.addWidget(rumor1)
+        hboxU.addWidget(rumor2)
+        vbox.addLayout(hboxU)
+
+        self.manje = QLabel("Manje cesti simptomi",self)
+        vbox.addWidget(self.manje)
+
+        #bol
+        hboxB = QtWidgets.QHBoxLayout()
+        lbol = QLabel("Bolovi u misicima: ")
+        rbol1= QtWidgets.QRadioButton("Da")
+        rbol2= QtWidgets.QRadioButton("Ne")
+        hboxB.addWidget(lbol)
+        hboxB.addWidget(rbol1)
+        hboxB.addWidget(rbol2)
+        vbox.addLayout(hboxB)
+
+        #upaljeno grlo
+        hboxUG = QtWidgets.QHBoxLayout()
+        lugrlo = QLabel("Upaljeno grlo: ")
+        rugrlo1= QtWidgets.QRadioButton("Da")
+        rugrlo2= QtWidgets.QRadioButton("Ne")
+        hboxUG.addWidget(lugrlo)
+        hboxUG.addWidget(rugrlo1)
+        hboxUG.addWidget(rugrlo2)
+        vbox.addLayout(hboxUG)
+
+        #dijareja
+        hboxD = QtWidgets.QHBoxLayout()
+        ldijareja = QLabel("Dijareja (proliv): ")
+        rdijareja1 = QtWidgets.QRadioButton("Da")
+        rdijareja2 = QtWidgets.QRadioButton("Ne")
+        hboxD.addWidget(ldijareja)
+        hboxD.addWidget(rdijareja1)
+        hboxD.addWidget(rdijareja2)
+        vbox.addLayout(hboxD)
+
+        #glavobolja
+        hboxG = QtWidgets.QHBoxLayout()
+        lglavobolja = QLabel("Glavobolja: ")
+        rglavobolja1 = QtWidgets.QRadioButton("Da")
+        rglavobolja2 = QtWidgets.QRadioButton("Ne")
+        hboxG.addWidget(lglavobolja)
+        hboxG.addWidget(rglavobolja1)
+        hboxG.addWidget(rglavobolja2)
+        vbox.addLayout(hboxG)
+ 
+        #gubitak cula
+        hboxC = QtWidgets.QHBoxLayout()
+        lcula = QLabel("Gubitak cula mirisa ili ukusa: ")
+        rcula1 = QtWidgets.QRadioButton("Cubitak cula mirisa")
+        rcula2 = QtWidgets.QRadioButton("Gubitak cula ukusa")
+        rcula3 = QtWidgets.QRadioButton("Nije doslo do gubitka cula mirisa ili ukusa")
+        #grupa = QtGui.QButtonGroup()
+        #rgrupa.addButton(rcula1)
+        #rgrupa.addButton(rcula2)
+        #rgrupa.addButton(rcula3)
+        
+        hboxC.addWidget(lcula)
+        hboxC.addWidget(rcula1)
+        hboxC.addWidget(rcula2)
+        hboxC.addWidget(rcula3)
+       # hboxC.addWidget(rgrupa)
+        vbox.addLayout(hboxC)
 
 
 app = QtWidgets.QApplication([])
